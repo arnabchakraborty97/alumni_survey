3ca74cc5732a495b3e65c2f80b068ec4586cae36
Arnab Chakraborty <arnab033chakraborty@gmail.com>
2019-03-05 11:24:15 +0530

    Fixed batch year values

--- resources/views/surveys/ExitCreate.blade.php
+++ resources/views/surveys/ExitCreate.blade.php
@@ -33,34 +33,34 @@
 				{{ Form::hidden('question[]', $questions[3]->id) }}
 				{{ Form::label('4', $questions[3]->body, ['class' => 'col-form-label font-weight-bold']) }}
 				<select id="4" name="4" class="form-control">
-					<option value="1998">1990</option>
-					<option value="1998">1991</option>
-					<option value="1998">1992</option>
-					<option value="1998">1993</option>
-					<option value="1998">1994</option>
-					<option value="1998">1995</option>
-					<option value="1998">1996</option>
-					<option value="1998">1997</option>
+					<option value="1990">1990</option>
+					<option value="1991">1991</option>
+					<option value="1992">1992</option>
+					<option value="1993">1993</option>
+					<option value="1994">1994</option>
+					<option value="1995">1995</option>
+					<option value="1996">1996</option>
+					<option value="1997">1997</option>
 					<option value="1998">1998</option>
-					<option value="1998">1999</option>
-					<option value="1998">2000</option>
-					<option value="1998">2001</option>
-					<option value="1998">2002</option>
-					<option value="1998">2003</option>
-					<option value="1998">2004</option>
-					<option value="1998">2005</option>
-					<option value="1998">2006</option>
-					<option value="1998">2007</option>
-					<option value="1998">2008</option>
-					<option value="1998">2009</option>
-					<option value="1998">2010</option>
-					<option value="1998">2011</option>
-					<option value="1998">2012</option>
-					<option value="1998">2013</option>
-					<option value="1998">2014</option>
-					<option value="1998">2015</option>
-					<option value="1998">2016</option>
-					<option value="1998">2017</option>
+					<option value="1999">1999</option>
+					<option value="2000">2000</option>
+					<option value="2001">2001</option>
+					<option value="2002">2002</option>
+					<option value="2003">2003</option>
+					<option value="2004">2004</option>
+					<option value="2005">2005</option>
+					<option value="2006">2006</option>
+					<option value="2007">2007</option>
+					<option value="2008">2008</option>
+					<option value="2009">2009</option>
+					<option value="2010">2010</option>
+					<option value="2011">2011</option>
+					<option value="2012">2012</option>
+					<option value="2013">2013</option>
+					<option value="2014">2014</option>
+					<option value="2015">2015</option>
+					<option value="2016">2016</option>
+					<option value="2017">2017</option>
 				</select>		
 
 				{{ Form::hidden('question[]', $questions[4]->id) }}
